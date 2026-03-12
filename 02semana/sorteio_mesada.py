6dd49d0e6922fcbc5f88aa48bc3a9b27a51216a6
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2026-03-12 17:57:15 -0300

    Add a new value to the mesada lottery options

--- 02semana/sorteio_mesada.py
+++ 02semana/sorteio_mesada.py
@@ -1,10 +1,10 @@
 from random import randint
-valores = [20, 40, 80, 100]
+valores = [20, 40, 60, 80, 100]
 filhos = ["HUGUINHO", "ZEZINHO", "LUIZINHO"]
 print("===================================")
 print("======== SORTEIO DA MESADA ========")
 print("===================================")
 for i in filhos:
-    valor = valores[randint(0, 3)]
+    valor = valores[randint(0, 4)]
     print(f"===   {i:10} : {valor:10}   ===")
 print("===================================")
