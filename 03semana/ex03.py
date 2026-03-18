aab1a4d9015c560bd890c0d6d9c948feaff5ca77
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2026-03-18 17:37:48 -0300

    Add value swapping program in ex03.py

---
+++ 03semana/ex03.py
@@ -0,0 +1,9 @@
+a = int(input("Informe um valor para A: "))
+b = int(input("Informe um valor para B: "))
+print("Invertendo os valores...\n")
+c = a
+a = b
+b = c
+print("Novo valor de A:", a)
+print("Novo valor de B:", b)
+print("Fim do Programa")
