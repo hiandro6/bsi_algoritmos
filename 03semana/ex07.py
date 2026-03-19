883f4f73dec7b52c73d84fd0e7463d0d7bd60519
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2026-03-19 17:18:06 -0300

    Add script to calculate wire needed for fencing

---
+++ 03semana/ex07.py
@@ -0,0 +1,4 @@
+#quantidade de arame para cercar uma fazenda
+perimetro = float(input("informe o perimetro da fazenda: "))
+quantidade_arame = perimetro * 5
+print(f"você irá precisar de {quantidade_arame:.2f}m de arame para cercar sua fazenda")
