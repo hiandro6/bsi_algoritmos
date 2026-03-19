d0585e20c7ababa23af36654aed328dcb115d7e9
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2026-03-19 17:04:23 -0300

    Add script to calculate area of a terrain

---
+++ 03semana/ex04.py
@@ -0,0 +1,6 @@
+# calculando a área de um terreno
+largura = float(input("informe a largura do ambiente: "))
+comprimento = float(input("informe o comprimento do ambiente: "))
+
+area = largura * comprimento
+print(f"a área do terreno é de {area:.2f} m²")
