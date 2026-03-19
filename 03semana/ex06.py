e2c2c53f8a2a643a2d8e248f23cc8c11d6a9ef95
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2026-03-19 17:11:59 -0300

    Add script to calculate area of a triangular plot

---
+++ 03semana/ex06.py
@@ -0,0 +1,6 @@
+# calculando a área de um terreno triangular
+largura = float(input("informe a largura do ambiente: "))
+comprimento = float(input("informe o comprimento do ambiente: "))
+
+area = (largura * comprimento) / 2
+print(f"a área do terreno é de {area:.2f} m²")
