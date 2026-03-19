4915bfc88366ffabb2d428e9ddf40be62404c887
Hiandro Alex <127314813+hiandro6@users.noreply.github.com>
2026-03-19 17:57:16 -0300

    Calculate perimeter based on width and length inputs

--- 03semana/ex07.py
+++ 03semana/ex07.py
@@ -1,4 +1,6 @@
 #quantidade de arame para cercar uma fazenda
-perimetro = float(input("informe o perimetro da fazenda: "))
+largura = float(input("informe o comprimento da fazenda: "))
+comprimento = float(input("informe a largura da fazenda: "))
+perimetro = (largura * 2) + (comprimento * 2)
 quantidade_arame = perimetro * 5
 print(f"você irá precisar de {quantidade_arame:.2f}m de arame para cercar sua fazenda")
